961fdad4cebd8b36bc647a126f0f6e5c250a8c25
mochinekoo <mochinekoo0113@gmail.com>
2026-09-01 05:05:46 +0900

    [add] プレイヤーのキー入力を追加しました

--- Player.cpp
+++ Player.cpp
@@ -7,13 +7,22 @@
 void Player::Init() {
 	fbx_ = new FBX("Asset/Player.fbx");
 	fbx_->Init();
+
+	transform_.velocity_ = {0.1, 0, 0.1};
 }
 
 void Player::Update() {
-	if (InputManager::CheckDownKey(DIK_SPACE)) {
+	if (InputManager::CheckDownKey(DIK_SPACE) || InputManager::IsControllerButtonPush(XINPUT_GAMEPAD_A)) {
 		Bullet* bullet = new Bullet();
 		bullet->SetLocation(transform_.location_);
 		ObjectManager::AddObject(bullet);
+	}
+
+	if (InputManager::CheckPushKey(DIK_LEFT) || InputManager::GetControllerTiltLeft().x <= -32767 * 0.5) {
+		transform_.location_.x -= transform_.velocity_.x;
+	}
+	if (InputManager::CheckPushKey(DIK_RIGHT) || InputManager::GetControllerTiltLeft().x >= 32767 * 0.5) {
+		transform_.location_.x += transform_.velocity_.x;
 	}
 
 	fbx_->SetTransform(transform_);
